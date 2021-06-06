aade3383da603b78530d75994e97177bd1e5b687
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2021-06-06 16:01:31 +0200

    Propojení stránek + možnost odhlášení

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
+import { RegistrationContext } from './../../lib/RegistrationContext';
 import './style.css';
 
 export const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { registrationState, setRegistrationState } =
+    useContext(RegistrationContext);
+
+  useEffect(
+    () => document.body.addEventListener('click', () => setMenuOpen(false)),
+    [],
+  );
 
   const handleOnClick = (e) => {
     setMenuOpen(true);
     e.stopPropagation();
   };
 
-  useEffect(
-    () => document.body.addEventListener('click', () => setMenuOpen(false)),
-    [],
-  );
+  const handleSignOut = () => {
+    setRegistrationState(null);
+  };
 
   return (
     <header className="header">
@@ -31,13 +38,36 @@ export const Header = () => {
             <HashLink to="/#o-projektu">O projektu</HashLink>
           </li>
 
-          <li className="menu-item">
-            <Link to="/prihlaseni">Přihlášení</Link>
-          </li>
+          {registrationState === null ? (
+            <>
+              <li className="menu-item">
+                <Link to="/prihlaseni">Přihlášení</Link>
+              </li>
 
-          <li className="menu-item sign-up">
-            <Link to="/registrace">Registrace</Link>
-          </li>
+              <li className="menu-item sign-up">
+                <Link to="/registrace">Registrace</Link>
+              </li>
+            </>
+          ) : registrationState.role === 'wheelchair' ? (
+            <>
+              <li className="menu-item">
+                <Link to="/">Seznam požadavků</Link>
+              </li>
+              <li className="menu-item sign-up">
+                <Link onClick={handleSignOut} to="/">
+                  Odhlásit se
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="menu-item sign-up">
+                <Link onClick={handleSignOut} to="/">
+                  Odhlásit se
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
---
+++ src/lib/RegistrationContext.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const RegistrationContext = React.createContext({
+  registrationState: null,
+  setRegistrationState: () => {},
+});
